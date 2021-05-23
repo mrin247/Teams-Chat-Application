47659b8035b78cee1606c15d1e021e1e84c32d3a
alexander-achilles <mrinmoym247@gmail.com>
2021-05-23 18:58:56 +0530

    Add Login functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import {ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-const App =( )=>{
-    return (
-        <ChatEngine
-          height="100vh"
-          projectID=" "
-          userName= "Admin"
-          userSecret="adminpassword"
-          renderChatFeed={(chatAppProps)=> <ChatFeed{...chatAppProps}/>}
-        />
-    )
-}
+const projectID = '1104af20-8f17-41b4-a65b-b4303108cdda';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
 
 
 export default App;
